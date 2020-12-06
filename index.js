f127629e775979216331ebb083553495dcf1688e
DarccMan <darcy3057@gmail.com>
2020-12-06 11:03:23 +1100

    Added CanvasRenderingContext2D.fillRoundRect and .strokeRoundRect functions for HTML

--- index.js
+++ index.js
@@ -1441,6 +1441,36 @@ if (F._data.canvas) {
     }
     return (output);
   }
+  CanvasRenderingContext2D.prototype.fillRoundRect = function (x, y, w, h, r) {
+    if (w < 2 * r) {
+      r = w / 2;
+    }
+    if (h < 2 * r) {
+      r = h / 2;
+    }
+    this.beginPath();
+    this.moveTo(x + r, y);
+    this.arcTo(x + w, y, x + w, y + h, r);
+    this.arcTo(x + w, y + h, x, y + h, r);
+    this.arcTo(x, y + h, x, y, r);
+    this.arcTo(x, y, x + w, y, r);
+    this.fill();
+  }
+  CanvasRenderingContext2D.prototype.strokeRoundRect = function (x, y, w, h, r) {
+    if (w < 2 * r) {
+      r = w / 2;
+    }
+    if (h < 2 * r) {
+      r = h / 2;
+    }
+    this.beginPath();
+    this.moveTo(x + r, y);
+    this.arcTo(x + w, y, x + w, y + h, r);
+    this.arcTo(x + w, y + h, x, y + h, r);
+    this.arcTo(x, y + h, x, y, r);
+    this.arcTo(x, y, x + w, y, r);
+    this.stroke();
+  }
 }
 
 
